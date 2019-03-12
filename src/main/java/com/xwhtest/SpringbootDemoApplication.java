7067b217e4b49537add460fd5df998f69a02a4d1
xwhard <xwhard@qq.com>
2019-03-12 16:53:32 +0800

    提交测试

--- src/main/java/com/xwhtest/SpringbootDemoApplication.java
+++ src/main/java/com/xwhtest/SpringbootDemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootDemoApplication.class, args);
-		System.out.println("springboot 启动成功");
+		System.out.println("springboot 启动成功!!!!!");
 	}
 
 }
